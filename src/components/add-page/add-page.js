d252d82c46e6dad957a548ab001d7d1331b764d8
Zachary Zboncak <zachary.zboncak@gmail.com>
2020-02-16 22:53:03 -0600

    adding to a users cellar is complete

--- src/components/add-page/add-page.js
+++ src/components/add-page/add-page.js
@@ -45,6 +45,7 @@ class AddPage extends React.Component {
                     beer_name={item.beer.beer_name}
                     beer_description={item.beer.beer_description}
                     beer_label={item.beer.beer_label}
+                    history={this.props.history}
                 />)
         }
     }
--- src/components/beerResult/beerResult.js
+++ src/components/beerResult/beerResult.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import './beerResult.css';
 import config from '../../config';
+import TokenService from '../../services/token-service';
 
 class BeerResult extends React.Component {
     
     handleAdd = (e) => {
         e.preventDefault();
         console.log(`You tried to add a beer with id ${this.props.bid}`);
-        let url = `${config.API_ENDPOINT}/add/${this.props.bid}`;
-        fetch(url)
-            .then(res => res.json())
-            .then(data => console.log(data))
+        let url = `${config.LOCAL_API_ENDPOINT}/cellar/add/${this.props.bid}`;
+        let options = {
+            method: 'post',
+            headers: {
+                'Authorization': `Bearer ${TokenService.getAuthToken()}`
+            }
+        }
+        fetch(url, options)
+            .then(res => this.props.history.push('/cellar'))
+            //.then(data => console.log(data))
             .catch(err => console.log(err))
     }
 
