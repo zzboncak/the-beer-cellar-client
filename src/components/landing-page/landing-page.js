3b5b08b4f90a030119e5cb0f37ed6e614635e195
Zachary Zboncak <zachary.zboncak@gmail.com>
2020-02-19 12:14:38 -0600

    hide login and register buttons when already logged in

--- src/components/landing-page/landing-page.js
+++ src/components/landing-page/landing-page.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './landing-page.css';
+import TokenService from '../../services/token-service';
 
 class LandingPage extends React.Component {
+    
+    renderAuthButtons = () => {
+        if(TokenService.getAuthToken()) {
+            return (
+                <div className="auth-buttons">
+                    <Link to='/cellar'>
+                        <button>See my cellar</button>
+                    </Link>
+                </div>
+            )
+        } else {
+            return (
+                <div className="auth-buttons">
+                    <Link to='/login'>
+                        <button>Login</button>
+                    </Link>
+                    {' '}
+                    <Link to='/register'>
+                        <button>Register</button>
+                    </Link>
+                </div>
+            )
+        }
+    }
+    
     render() {
+        let authButtons = this.renderAuthButtons();
         return(
             <div>
                 <nav className="nav">
@@ -11,15 +38,7 @@ class LandingPage extends React.Component {
                         <button className="demo-button">Demo</button>
                     </Link>
 
-                    <div className="auth-buttons">
-                        <Link to='/login'>
-                            <button>Login</button>
-                        </Link>
-                        {' '}
-                        <Link to='register'>
-                            <button>Register</button>
-                        </Link>
-                    </div>
+                    {authButtons}
                 </nav>
 
                 <main>
