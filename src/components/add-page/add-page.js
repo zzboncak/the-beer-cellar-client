f64c14ec2d66220db4c57c8326c279b933e1bf5d
Zachary Zboncak <zachary.zboncak@gmail.com>
2020-11-22 15:24:20 -0600

    refactored search page for functional component

--- src/components/add-page/add-page.js
+++ src/components/add-page/add-page.js
@@ -1,69 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import config from "../../config";
 import "./add-page.css";
 import BeerResult from "../beerResult/beerResult";
 import UntappdLogo from "../../images/pbu_80_grey.png";
 
-class AddPage extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			userSearch: "",
-			beersFromSearch: [],
-		};
-	}
+const AddPageFunction = (props) => {
+	const [userSearch, setUserSearch] = useState("");
+	const [beersFromSearch, setBeersFromSearch] = useState([]);
+	const [searching, setSearching] = useState(false);
+	const [touched, setTouched] = useState(false);
 
-	onSearchChange = (e) => {
-		this.setState({
-			userSearch: e.target.value,
-		});
-	};
-
-	handleAddFormSubmit = (e) => {
+	const handleAddFormSubmit = (e) => {
 		e.preventDefault();
-		let searchTerm = this.state.userSearch.replace(/ /g, "+");
+		setSearching(true);
+		setTouched(true);
+		let searchTerm = userSearch.replace(/ /g, "+");
 		let url = `${config.API_ENDPOINT}/search/${searchTerm}`;
 		fetch(url)
 			.then((res) => res.json())
 			.then((data) => {
-				this.setState({
-					userSearch: "",
-					beersFromSearch: data.response.beers.items,
-				});
+				setSearching(false)
+				setUserSearch("");
+				setBeersFromSearch(data.response.beers.items);
 			});
 	};
 
-	renderSearchResults() {
-		if (this.state.beersFromSearch === []) {
-			return (
-				<div className="results-section">
-					Beers will appear below from your search
-				</div>
-			);
-		} else {
-			return this.state.beersFromSearch.map((item, i) => (
-				<BeerResult
-					key={i}
-					bid={item.beer.bid}
-					beer_name={item.beer.beer_name}
-					beer_description={item.beer.beer_description}
-					beer_label={item.beer.beer_label}
-					brewery_name={item.brewery.brewery_name}
-					history={this.props.history}
-				/>
-			));
-		}
-	}
+	const onSearchChange = (e) => {
+		setUserSearch(e.target.value);
+	};
 
-	render() {
-		let searchResults = this.renderSearchResults();
-
-		return (
-			<div className="add-page">
+	return (
+		<div className="add-page">
 				<h2 className="page-title">Add a beer</h2>
 
-				<form className="add-form" onSubmit={this.handleAddFormSubmit}>
+				<form className="add-form" onSubmit={handleAddFormSubmit}>
 					<fieldset className="beer-info">
 						<label htmlFor="username">Search:</label>
 						<br />
@@ -71,8 +42,8 @@ class AddPage extends React.Component {
 							name="username"
 							type="search"
 							placeholder="ex: Pliny the Elder"
-							value={this.state.userSearch}
-							onChange={this.onSearchChange}
+							value={userSearch}
+							onChange={e => onSearchChange(e)}
 						/>
 
 						<div className="button-container">
@@ -88,7 +59,24 @@ class AddPage extends React.Component {
 					</fieldset>
 				</form>
 
-				<section className="results-section">{searchResults}</section>
+				<section className="results-section">
+					{searching 
+						? <div>Loading your beers</div> 
+						: (beersFromSearch.length === 0 && !searching && touched)
+						? <div>No beers found</div> 
+						: beersFromSearch.map((item, i) => (
+							<BeerResult
+								key={i}
+								bid={item.beer.bid}
+								beer_name={item.beer.beer_name}
+								beer_description={item.beer.beer_description}
+								beer_label={item.beer.beer_label}
+								brewery_name={item.brewery.brewery_name}
+								history={props.history}
+							/>	
+						))
+					}
+				</section>
 
 				<img
 					id="untappd-logo"
@@ -96,8 +84,7 @@ class AddPage extends React.Component {
 					alt="powered by Untappd"
 				/>
 			</div>
-		);
-	}
+	)
 }
 
-export default AddPage;
+export default AddPageFunction;
